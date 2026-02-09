15fe5925f4ee37b13dce6640ac2b73cc71aa5e77
85bztqjfnv-code <aaronlin931111@outlook.com>
2026-02-09 09:03:21 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+// TODO: Replace with your actual Google Apps Script Web App URL after deployment
+const API_URL = "https://script.google.com/macros/s/AKfycbyoFkZkHLaehEj953pdbkpR8_t08Sp1RyGCFWcOVcGbw8gBGJZNQX1MLqA8p8552gA1/exec";
