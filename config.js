f5bd90087e1f3eeb286b708e380734a73f206c66
85bztqjfnv-code <aaronlin931111@outlook.com>
2026-04-07 19:07:30 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 // TODO: Replace with your actual Google Apps Script Web App URL after deployment
-window.API_URL = "https://script.google.com/macros/s/AKfycbx06gFyfoAGJfqMBCHJDYRYgZ31bDXAXO8kpfMOmLDAheLj9QhiVLHLkm1rUI_NmA/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbxF4XlSQHbTWNV7tSeNERaKSuF4p8SW_LcRcDUZfknVDHTeb_9u4sCL6wTGCV_lX7jx/exec";
 var API_URL = window.API_URL;
